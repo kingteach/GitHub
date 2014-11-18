9f92fb404999939bd96dad3688e14710ec6acecf
kingteach <708895310@qq.com>
2014-11-18 09:35:05 +0800

    HtmlSaveAs
    DownLoad HtmlFile like HtmlSaveAs

---
+++ 网页另存为/HtmlSave/HtmlSave/FrmMain.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace HtmlSave
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pnlContent.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(558, 305);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRead.Location = new System.Drawing.Point(302, 5);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "1.读取地址";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(392, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "2.开始下载";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Controls.Add(this.webBrowser1);
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(0, 0);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(558, 305);
+            this.pnlContent.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtConsole);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 338);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(558, 88);
+            this.panel2.TabIndex = 8;
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsole.Location = new System.Drawing.Point(0, 0);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.Size = new System.Drawing.Size(558, 88);
+            this.txtConsole.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.progressBar1);
+            this.panel3.Controls.Add(this.btnStop);
+            this.panel3.Controls.Add(this.btnStart);
+            this.panel3.Controls.Add(this.btnRead);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 305);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(558, 33);
+            this.panel3.TabIndex = 9;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 6);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(284, 21);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(480, 5);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "3.停止下载";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 426);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "FrmMain";
+            this.Text = "网页下载";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.pnlContent.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlContent;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ 网页另存为/HtmlSave/HtmlSave/FrmMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HtmlSave
+{
+    public partial class FrmMain : Form
+    {
+        #region 变量
+        private System.Timers.Timer t;
+        private bool hasChoosePath = false;// 标记是否完成第一次选择路径
+        private bool beginSendKey = false;// 标记是否可以发送保存键
+        /// <summary>
+        /// 需要下载的地址
+        /// </summary>
+        private List<string> urls = new List<string>();
+        #endregion
+
+        #region 构造函数
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 读取地址
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // 在此加载地址
+            urls = new List<string>() { 
+                "http://www.baidu.com/",
+                 "http://www.cnblogs.com/", 
+                "http://www.qq.com/",
+                "http://www.taobao.com/",
+               };
+            progressBar1.Maximum = urls.Count;
+            progressBar1.Value = 0;//设置当前值
+            progressBar1.Step = 1;//设置没次增长多少
+
+            WriteMsg("", "==============读取地址成功，可以点击按钮下载了==============");
+        }
+
+        /// <summary>
+        /// 开始下载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+            hasChoosePath = false;
+            StarTimer();
+            progressBar1.Value = 0;
+            int i = 0;
+            foreach (var url in urls)
+            {
+                bool b = this.Navigate(url);
+                if (!b)
+                {
+                    WriteMsg(url, "导航失败");
+                    continue;
+                }
+
+                if (WaitWebPageLoad())
+                {
+                    beginSendKey = true;
+                    this.TopLevel = true;
+                    webBrowser1.ShowSaveAsDialog();
+                    i++;
+                    WriteMsg(url, "下载成功");
+                    hasChoosePath = true;
+                    beginSendKey = false;
+                }
+                else
+                {
+                    WriteMsg(url, "等待加载失败");
+                }
+                progressBar1.Value += 1;
+            }
+            StopTimer();
+            progressBar1.Value = 0;
+            WriteMsg("", string.Format("==============成功下载：{0}个网址,失败：{1}===============", i, urls.Count - i));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+            WriteMsg("", "已经停止下载");
+        }
+
+        #region 定时器
+
+        private void StarTimer()
+        {
+            t = new System.Timers.Timer(600);//实例化Timer类，设置时间间隔
+            t.AutoReset = true;//设置是执行一次（false）还是一直执行(true) 
+            t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件
+            t.Elapsed += t_Elapsed;
+            t.Start();
+        }
+
+        private void StopTimer()
+        {
+            if (t != null)
+            {
+                t.Enabled = false;
+                t.Dispose();
+                t.Stop();
+            }
+        }
+
+        private void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (hasChoosePath && beginSendKey)
+            {
+                SendKeys.SendWait("%y");
+                SendKeys.SendWait("%y");
+                SendKeys.SendWait("%s");
+            }
+        }
+
+        #endregion
+
+        #region 判断页面是否加载完成
+        private void Delay(int Millisecond) //延迟系统时间，但系统又能同时能执行其它任务；
+        {
+            DateTime current = DateTime.Now;
+            while (current.AddMilliseconds(Millisecond) > DateTime.Now)
+            {
+                Application.DoEvents();//转让控制权            
+            }
+            return;
+        }
+
+        private bool WaitWebPageLoad()
+        {
+            try
+            {
+                int i = 0;
+                string sUrl = "";
+                while (true)
+                {
+                    Delay(50);  //系统延迟50毫秒，够少了吧！             
+                    if (webBrowser1.ReadyState == WebBrowserReadyState.Complete) //先判断是否发生完成事件。
+                    {
+                        if (!webBrowser1.IsBusy) //再判断是浏览器是否繁忙                  
+                        {
+                            i = i + 1;
+                            if (i == 2)   //为什么 是2呢？因为每次加载frame完成时就会置IsBusy为false,未完成就就置IsBusy为false，你想一想，加载一次，然后再一次，再一次...... 最后一次.......
+                            {
+                                sUrl = webBrowser1.Url.ToString();
+                                if (sUrl.Contains("res")) //这是判断没有网络的情况下                           
+                                {
+                                    return false;
+                                }
+
+                                else
+                                {
+                                    return true;
+                                }
+                            }
+                            continue;
+                        }
+                        i = 0;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Helper
+        // Navigates to the given URL if it is valid.
+        private bool Navigate(string url)
+        {
+            if (String.IsNullOrEmpty(url)) return false;
+            if (url.Equals("about:blank")) return false;
+            if (!url.StartsWith("http://") &&
+                !url.StartsWith("https://"))
+            {
+                url = "http://" + url;
+            }
+            try
+            {
+                webBrowser1.Navigate(new Uri(url));
+                return true;
+            }
+            catch (System.UriFormatException)
+            {
+                return false;
+            }
+        }
+
+        private void WriteMsg(string url, string msg)
+        {
+            txtConsole.Text += string.Format("{0}-----{1}\r\n", url, msg);
+        }
+
+        #endregion
+    }
+}
